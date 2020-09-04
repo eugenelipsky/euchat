db297d83dd6f41f171bf22a88bbdaa3fdb87d973
Eugene <zvlipskiy@gmail.com>
2020-09-04 17:26:43 +0300

    Some config changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,10 @@ const colors = require('vuetify/es5/util/colors').default
 module.exports = {
   mode: 'universal',
   telemetry: false,
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  },
   /*
   ** Headers of the page
   */
